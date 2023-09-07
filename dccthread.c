fc8132d592ce9e012d414ee77f71244a534f0958
joaomlg <joaomarcos1999jm@gmail.com>
2023-09-07 10:44:22 -0300

    refactor: round robin and quantum variables name changed

--- dccthread.c
+++ dccthread.c
@@ -11,8 +11,8 @@
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-#define THREAD_YIELD_TIME_NSEC 10000000
-#define THREAD_YIELD_TIMER_SIGNAL SIGUSR1
+#define QUANTUM_TIME_NSEC 10000000
+#define ROUND_ROBIN_TIMER_SIGNAL SIGUSR1
 
 typedef struct dccthread{
 	int id;
@@ -29,13 +29,13 @@ dccthread_t *manager_thread;
 
 int threads_counter = 0;
 
-timer_t thread_yield_timer_id;
-struct itimerspec thread_yield_timer_spec;
-struct sigevent thread_yield_timer_sigevent;
-struct sigaction thread_yield_sigaction;
+timer_t round_robin_timer_id;
+struct itimerspec round_robin_timer_spec;
+struct sigevent round_robin_timer_sigevent;
+struct sigaction round_robin_sigaction;
 sigset_t mask_signals_set;
 
-static void thread_yield_sigaction_handler(int signo, siginfo_t *info, void *context) {
+static void round_robin_sigaction_handler(int signo, siginfo_t *info, void *context) {
     dccthread_yield();
 }
 
@@ -43,19 +43,19 @@ void dccthread_init(void (*func)(int), int param) {
     ready_list = dlist_create();
     waiting_list = dlist_create();
 
-    thread_yield_timer_sigevent.sigev_notify = SIGEV_SIGNAL;
-    thread_yield_timer_sigevent.sigev_signo = THREAD_YIELD_TIMER_SIGNAL;
+    round_robin_timer_sigevent.sigev_notify = SIGEV_SIGNAL;
+    round_robin_timer_sigevent.sigev_signo = ROUND_ROBIN_TIMER_SIGNAL;
 
-    if (timer_create(CLOCK_PROCESS_CPUTIME_ID, &thread_yield_timer_sigevent, &thread_yield_timer_id) == -1) {
+    if (timer_create(CLOCK_PROCESS_CPUTIME_ID, &round_robin_timer_sigevent, &round_robin_timer_id) == -1) {
         handle_error("Cannot create timer to force thread yield");
     }
 
-    thread_yield_timer_spec.it_value.tv_nsec = THREAD_YIELD_TIME_NSEC;
+    round_robin_timer_spec.it_value.tv_nsec = QUANTUM_TIME_NSEC;
 
-    thread_yield_sigaction.sa_flags = SA_SIGINFO;
-    thread_yield_sigaction.sa_sigaction = thread_yield_sigaction_handler;
+    round_robin_sigaction.sa_flags = SA_SIGINFO;
+    round_robin_sigaction.sa_sigaction = round_robin_sigaction_handler;
 
-    if (sigaction(THREAD_YIELD_TIMER_SIGNAL, &thread_yield_sigaction, NULL) == -1) {
+    if (sigaction(ROUND_ROBIN_TIMER_SIGNAL, &round_robin_sigaction, NULL) == -1) {
         handle_error("Cannot set action for thread yield timer signal");
     }
 
@@ -63,7 +63,7 @@ void dccthread_init(void (*func)(int), int param) {
         handle_error("Cannot initiate an empty mask signals set");
     }
 
-    if (sigaddset(&mask_signals_set, THREAD_YIELD_TIMER_SIGNAL) == -1) {
+    if (sigaddset(&mask_signals_set, ROUND_ROBIN_TIMER_SIGNAL) == -1) {
         handle_error("Cannot add thread yield timer signal to mask signals set");
     }
 
@@ -92,7 +92,7 @@ void dccthread_init(void (*func)(int), int param) {
         dccthread_t *next_thread = (dccthread_t*) malloc(sizeof(dccthread_t));
         next_thread = ready_list->head->data;
 
-        if (timer_settime(thread_yield_timer_id, 0, &thread_yield_timer_spec, NULL) == -1) {
+        if (timer_settime(round_robin_timer_id, 0, &round_robin_timer_spec, NULL) == -1) {
             handle_error("Cannot set timer to yield thread");
         }
 
